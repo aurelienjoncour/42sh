1a1f8ef9788ad9612b46a5b0e5c7a8e4e7e7152c
Aurelien Joncour <aurelien.joncour@epitech.eu>
2020-04-27 15:46:56 +0200

    feat(parser.c:parser.h) adding structure parser and token and tab for parsing

---
+++ src/parser.c
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_42sh_2019
+** File description:
+** parser
+*/
+
+#include <stdlib.h>
+#include "parser.h"
+
+parser_t delimit[] =
+{
+    {" ", "", D_NORMAL, ID_SPACE},
+    {"\t", "", D_NORMAL, ID_TAB},
+    {"\"", "\"", D_DELIM, ID_DOUBLE_QUOTE},
+    {"'", "'", D_DELIM, ID_SIMPLE_QUOTE},
+    {"`", "`", D_DELIM, ID_BACK_QUOTE},
+    {"(", ")", D_DELIM, ID_PARENTHESE},
+    {">&", "", D_NORMAL, ID_REDIRECTION},
+    {">>", "", D_GET, ID_DOUBLE_RIGHT},
+    {">", "", D_GET, ID_RIGHT},
+    {"<<", "", D_GET, ID_DOUBLE_LEFT},
+    {"<", "", D_GET, ID_LEFT},
+    {"||", "", D_SEPARATOR, ID_OR},
+    {"&&", "", D_SEPARATOR, ID_AND},
+    {";", "", D_SEPARATOR, ID_SEP},
+    {"|", "", D_GET, ID_PIPE},
+    {"&", "", D_GET, ID_BACKGROUND},
+    {NULL, NULL, ID_WIHOUT, ID_WIHOUT},
+}
