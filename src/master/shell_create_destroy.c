649403c353b0339a96a78ca396a2017be3ebd1d8
Vincent Andrieu <vincent.andrieu@epitech.eu>
2020-05-11 14:18:05 +0200

    Add create & detroy alias env

--- src/master/shell_create_destroy.c
+++ src/master/shell_create_destroy.c
@@ -9,21 +9,20 @@
 
 int shell_create(shell_t *shell, char **env)
 {
-    if (my_env_create(&shell->env, env) != EXIT_SUCCESS) {
+    if (my_env_create(&shell->env, env) != EXIT_SUCCESS)
         return EXIT_ERROR;
-    }
-    if (my_env_create(&shell->local, env) != EXIT_SUCCESS) {
+    if (my_env_create(&shell->local, env) != EXIT_SUCCESS)
         return EXIT_ERROR;
-    }
+    if (my_env_create(&shell->alias, env) != EXIT_SUCCESS)
+        return EXIT_ERROR;
     shell->prev_path = NULL;
     shell->exit = false;
     shell->exit_status = EXIT_SUCCESS;
     shell->fd.stdin = dup(0);
     shell->fd.stdout = dup(1);
     shell->fd.prev_pipein = -1;
-    if (shell->fd.stdin == -1 || shell->fd.stdout == -1) {
+    if (shell->fd.stdin == -1 || shell->fd.stdout == -1)
         return puterr("dup : fail\n", EXIT_ERROR);
-    }
     return EXIT_SUCCESS;
 }
 
@@ -31,6 +30,7 @@ int shell_destroy(shell_t *shell)
 {
     my_env_destroy(&shell->env);
     my_env_destroy(&shell->local);
+    my_env_destroy(&shell->alias);
     free(shell->prev_path);
     close(shell->fd.stdin);
     close(shell->fd.stdout);
