2da08b7179a379b17e08fc899d2e67f247ba9e9c
Vincent Andrieu <vincent.andrieu@epitech.eu>
2020-05-12 18:10:38 +0200

    Fix convert array to str

--- src/cmd/builtin/cmd/my_alias.c
+++ src/cmd/builtin/cmd/my_alias.c
@@ -31,15 +31,23 @@ static char *my_array_to_str(char **tab)
 {
     int size = 0;
     char *str;
+    int i_tab = 0;
 
-    for (int i = 0; tab[i] != NULL; i++)
-        size += my_strlen(tab[i]);
-    str = malloc(sizeof(char) * size + 1);
+    for (; tab[i_tab] != NULL; i_tab++)
+        size += my_strlen(tab[i_tab]) + 1;
+    str = malloc(sizeof(char) * size + 1 + (i_tab > 1 ? 2 : 0));
     if (str == NULL)
         return NULL;
-    str[0] = '\0';
-    for (int i = 0; tab[i] != NULL; i++)
+    if (i_tab > 1)
+        str[1] = '\0';
+    str[0] = i_tab > 1 ? '(' : '\0';
+    for (int i = 0; tab[i] != NULL; i++) {
         my_strcat(str, tab[i]);
+        my_strcat(str, " ");
+    }
+    str[my_strlen(str) - 1] = '\0';
+    if (i_tab > 1)
+        my_strcat(str, ")");
     return str;
 }
 
