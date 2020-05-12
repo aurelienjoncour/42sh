7e8ac933f8d4563148dbb51349c6c431ab2bbbaa
Vincent Andrieu <vincent.andrieu@epitech.eu>
2020-05-12 15:52:44 +0200

    Fix errors & prints

--- src/cmd/builtin/cmd/my_alias.c
+++ src/cmd/builtin/cmd/my_alias.c
@@ -10,12 +10,17 @@
 #include "my.h"
 #include "shell_t.h"
 
-static int print_alias(char *label, env_t *alias)
+static int print_alias(char *label, env_t *alias, bool print_label)
 {
     char *str = my_env_get_value(alias, label);
 
     if (str == NULL)
         return EXIT_SUCCESS;
+    if (print_label) {
+        my_putstr(label);
+        my_putstr("\t");
+        free(label);
+    }
     my_putstr(str);
     my_putstr("\n");
     free(str);
@@ -24,8 +29,12 @@ static int print_alias(char *label, env_t *alias)
 
 static char *my_array_to_str(char **tab)
 {
-    char *str = malloc(sizeof(char) * word_array_len(tab));
+    int size = 0;
+    char *str;
 
+    for (int i = 0; tab[i] != NULL; i++)
+        size += my_strlen(tab[i]);
+    str = malloc(sizeof(char) * size);
     if (str == NULL)
         return NULL;
     str[0] = '\0';
@@ -85,11 +94,13 @@ int my_alias(char **cmd, shell_t *shell)
     if (get_alias_opt(argc, cmd))
         return EXIT_SUCCESS;
     if (argc == 1) {
-        my_env_display(&shell->alias);
+        for (int i = 0; i < shell->alias.size; i++)
+            print_alias(my_env_get_var_label(shell->alias.var[i]),
+                        &shell->alias, true);
         return EXIT_SUCCESS;
     }
     if (argc == 2)
-        return print_alias(cmd[1], &shell->alias);
+        return print_alias(cmd[1], &shell->alias, false);
     target_cmd = my_array_to_str(cmd + 2);
     if (target_cmd == NULL)
         return EXIT_ERROR;
