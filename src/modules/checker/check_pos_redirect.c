11eaef8c33629e143768dd5ae3e01cadb060e7e0
RACAUD Simon <simonracaud@gmail.com>
2020-05-13 16:12:22 +0200

    Feat(modules/checker) check position file redirection in pipe

--- src/modules/checker/check_pos_redirect.c
+++ src/modules/checker/check_pos_redirect.c
@@ -7,40 +7,10 @@
 
 #include "shell.h"
 
-static const char PIPE_CHAR = '|';
-
-static const char *REDIRECT_PATT[] =
-{
-    "<",
-    "<<",
-    ">>",
-    ">",
-    NULL
-};
-
-static const int NO_TYPE = 0;
-static const int TYPE_IN = 1;
-static const int TYPE_OUT = 2;
-
 static const char *ERR_AMBIG_IN = "Ambiguous input redirect.\n";
 static const char *ERR_AMBIG_OUT = "Ambiguous output redirect.\n";
 
-static int get_type_redirect_pattern(const char *str)
-{
-    int res;
-
-    for (size_t i = 0; REDIRECT_PATT[i] != NULL; i++) {
-        res = my_strncmp(REDIRECT_PATT[i], str, my_strlen(REDIRECT_PATT[i]));
-        if (res == 0 && i <= 1) {
-            return TYPE_IN;
-        } else if (res == 0) {
-            return TYPE_OUT;
-        }
-    }
-    return NO_TYPE;
-}
-
-static int count_pipe(const char *sub_entry)
+static int count_pipe(c)
 {
     int count = 0;
 
@@ -52,24 +22,36 @@ static int count_pipe(const char *sub_entry)
     return count;
 }
 
-bool check_position_redirection_in_pipe(const char *sub_entry)
+static bool check_error(token_t *ptr, bool have_rd_right, in nb_pipe)
 {
-    int max_nb_pipe = count_pipe(sub_entry);
-    int type;
-    int nb_pipe = 0;
+    if (ptr->id == ID_PIPE && have_rd_right == true) {
+        my_putstr_error(ERR_AMBIG_OUT);
+        return true;
+    }
+    if ((ptr->id == ID_DOUBLE_LEFT || ptr->id == ID_LEFT) && nb_pipe > 0) {
+        my_putstr_error(ERR_AMBIG_IN);
+        return true;
+    }
+    return false;
+}
 
-    for (size_t i = 0; sub_entry[i] != '\0'; i++) {
-        if (sub_entry[i] == PIPE_CHAR) {
+bool check_position_redirection_in_pipe(cmd_t *cmd)
+{
+    int nb_pipe = 0;
+    bool have_rd_right = false;
+
+    for (token_t *ptr = cmd->begin; ptr != NULL; ptr = ptr->next) {
+        if (ptr->id == ID_PIPE) {
             nb_pipe++;
-            continue;
+        } else if (ptr->type == D_SEPARATOR) {
+            nb_pipe = 0;
+            have_rd_right = false;
         }
-        type = get_type_redirect_pattern(&sub_entry[i]);
-        if (type == TYPE_IN && nb_pipe > 0) {
-            my_putstr_error(ERR_AMBIG_IN);
+        if (check_error(ptr, have_rd_right, nb_pipe) == true) {
             return true;
-        } else if (type == TYPE_OUT && nb_pipe != max_nb_pipe) {
-            my_putstr_error(ERR_AMBIG_OUT);
-            return true;
+        }
+        if (ptr->id == ID_DOUBLE_RIGHT || ptr->id == ID_RIGHT) {
+            have_rd_right = true;
         }
     }
     return false;
