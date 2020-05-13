a6839f3afacb6add03a9a7cc79410d3f7304696a
RACAUD Simon <simonracaud@gmail.com>
2020-05-13 16:13:08 +0200

    Feat(module/checker) check have empty pipe

--- src/modules/checker/entry_checker.c
+++ src/modules/checker/entry_checker.c
@@ -7,10 +7,28 @@
 
 #include "shell.h"
 
+static const char *ERR_PIPE_NULL_CMD = "Invalid null command.\n";
+
+static bool check_have_empty_pipe(cmd_t *cmd)
+{
+    for (token_t *ptr = cmd->begin; ptr != NULL; ptr = ptr->next) {
+        if (ptr->id == ID_PIPE && (!ptr->next
+                || (ptr->next->type == D_SEPARATOR
+                    || ptr->next->id == ID_PIPE)) {
+            my_putstr(ERR_NULL_CMD);
+            return true;
+        }
+    }
+    return false;
+}
+
 int entry_checker(shell_t *shell, cmd_t *cmd)
 {
-    // if (check_position_redirection_in_pipe(sub_entry)) {
-    //     return EXIT_ERROR;
-    // }
+    if (check_have_empty_pipe(cmd)) {
+        shell->exit_status = ERROR_STATUS;
+    }
+    if (check_position_redirection_in_pipe(cmd)) {
+        shell->exit_status = ERROR_STATUS;
+    }
     return EXIT_SUCCESS;
 }
