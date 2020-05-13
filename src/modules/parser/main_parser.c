99b3b50b2dd3662f37940650e0758f7a1deb2cc8
RACAUD Simon <simonracaud@gmail.com>
2020-05-13 14:37:06 +0200

    Feat(modules/parser) At the end : clean list -> remove empty nodes

--- src/modules/parser/main_parser.c
+++ src/modules/parser/main_parser.c
@@ -25,6 +25,21 @@ static token_t *get_last_token_and_size(token_t *start, size_t *size)
     return ptr;
 }
 
+static void token_list_clean(token_t *start)
+{
+    token_t *next;
+
+    for (token_t *i = start; i != NULL;) {
+        if (i->token[0] == '\0' || i->id == ID_SPACE || i->id == ID_TAB) {
+            next = i->next;
+            rm_node(i);
+            i = next;
+        }
+        if (i != NULL)
+            i = i->next;
+    }
+}
+
 cmd_t *parse_entry(char *entry)
 {
     cmd_t *cmd = malloc(sizeof(cmd_t));
@@ -33,6 +48,7 @@ cmd_t *parse_entry(char *entry)
         return NULL;
     }
     cmd->begin = tokeniser(entry);
+    token_list_clean(cmd->begin);
     if (!cmd->begin) {
         free(cmd);
         return NULL;
