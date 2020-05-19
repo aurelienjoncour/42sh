61e529f9510889b843aa3ffdbeaf561e3b0586ae
RACAUD Simon <simonracaud@gmail.com>
2020-05-19 15:23:03 +0200

    Feat(modules.checker) check_parenthesis : don't show error => script struct (if, elseif, foreach)

--- src/modules/checker/check_parenthesis.c
+++ src/modules/checker/check_parenthesis.c
@@ -6,26 +6,68 @@
 */
 
 #include "shell.h"
-
 static const char *ERR_MISS_PARENTHESIS = "Too many %c's.\n";
 static const char *ERR_BAD_PARENTHESIS = "Badly placed ()'s.\n";
+
+static const char *SCRIPT_TOKEN[] =
+{
+    "if",
+    "elif",
+    "foreach",
+    NULL
+};
+
+static bool is_scripting_token(token_t *ptr)
+{
+    if (ptr->id != ID_WITHOUT) {
+        return false;
+    }
+    for (size_t i = 0; SCRIPT_TOKEN[i] != NULL; i++) {
+        if (strcmp(SCRIPT_TOKEN[i], ptr->token) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+static bool check_token(token_t *ptr, bool *have_text, bool *have_parenth,
+bool *is_script)
+{
+    if ((is_separator(ptr) && *have_parenth == true && *have_text == true)) {
+        return true;
+    } else if (is_scripting_token(ptr) == true) {
+        *is_script = true;
+    }
+    if (*is_script == false && is_text_token(ptr, false) == true)
+        *have_text = true;
+    if (is_separator(ptr) == true) {
+        *have_parenth = false;
+        *have_text = false;
+        *is_script = false;
+    } else if (ptr->id == ID_PARENTHESE) {
+        *have_parenth = true;
+    }
+    if (ptr->id == ID_PARENTHESE && is_script == true) {
+        while (ptr != NULL && is_separator(ptr) == false) {
+            ptr = ptr->next;
+        }
+    }
+    return false;
+}
 
 static bool check_parenthesis_position(cmd_t *cmd)
 {
     bool have_parenth = false;
     bool have_text = false;
+    bool is_script = false;
 
     for (token_t *ptr = cmd->begin; ptr != NULL; ptr = ptr->next) {
-        if (is_separator(ptr) && have_parenth == true && have_text == true) {
+        if (check_token(ptr, &have_text, &have_parenth, &is_script)) {
             break;
         }
-        if (is_text_token(ptr, false) == true)
-            have_text = true;
-        if (is_separator(ptr) == true) {
-            have_parenth = false;
-            have_text = false;
-        } else if (ptr->id == ID_PARENTHESE)
-            have_parenth = true;
+        if (ptr == NULL) {
+            break;
+        }
     }
     if (have_parenth == true && have_text == true) {
         my_putstr_error(ERR_BAD_PARENTHESIS);
@@ -37,16 +79,19 @@ static bool check_parenthesis_position(cmd_t *cmd)
 static bool check_parenthesis_serie(cmd_t *cmd)
 {
     size_t count_parenth = 0;
+    bool have_script = false;
 
     for (token_t *ptr = cmd->begin; ptr != NULL; ptr = ptr->next) {
         if (is_separator(ptr) && count_parenth > 1) {
             break;
         }
+        if (is_scripting_token(ptr) == true)
+            have_script = true;
         if (is_separator(ptr) == true) {
             count_parenth = 0;
-        } else if (ptr->id == ID_PARENTHESE) {
+            have_script = true;
+        } else if (ptr->id == ID_PARENTHESE && !have_script)
             count_parenth++;
-        }
     }
     if (count_parenth > 1) {
         fprintf(stderr, ERR_MISS_PARENTHESIS, ')');
