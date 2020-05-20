02cb70dd53fa38ce6e30e56c0a8a1b92c1d53672
RACAUD Simon <simonracaud@gmail.com>
2020-05-20 15:08:48 +0200

    Fix(entry.bool_operator) If && is false => execute next || operator

--- src/entry/bool_op/shell_exec_boolop.c
+++ src/entry/bool_op/shell_exec_boolop.c
@@ -12,32 +12,42 @@ static token_t *get_next_separator(token_t *ptr)
     if (ptr == NULL) {
         return NULL;
     }
-    for (; ptr != NULL; ptr = ptr->next) {
+    ptr = ptr->next;
+    while (ptr != NULL) {
         if (ptr->id == ID_OR || ptr->id == ID_AND) {
             return ptr;
         }
+        ptr = ptr->next;
     }
     return NULL;
 }
 
-static bool check_break_loop(token_t *ptr, shell_t *shell)
+static int check_break_loop(token_t *ptr, shell_t *shell)
 {
+    int ret = 0;
+
     if (ptr != NULL && ptr->id == ID_OR) {
-        if (shell->exit_status != 0) {
-            return false;
+        if (shell->exit_status == 0) {
+            return -1;
         }
     } else if (ptr != NULL && ptr->id == ID_AND) {
         if (shell->exit_status == 0) {
-            return false;
+            return 0;
         }
+        while (ptr != NULL && ptr->id != ID_OR) {
+            ret++;
+            ptr = get_next_separator(ptr);
+        }
+        return ret;
     }
-    return true;
+    return 0;
 }
 
 int shell_exec_boolop(shell_t *shell, cmd_t *pipe_cmd)
 {
     cmd_t **bool_cmd = split_cmd_list(pipe_cmd, ID_OR, ID_AND);
     token_t *ptr = pipe_cmd->begin;
+    int ret;
 
     for (size_t i = 0; bool_cmd[i] != NULL; i++) {
         if (bool_cmd[i]->begin == NULL)
@@ -46,7 +56,10 @@ int shell_exec_boolop(shell_t *shell, cmd_t *pipe_cmd)
             return EXIT_ERROR;
         }
         ptr = get_next_separator(ptr);
-        if (check_break_loop(ptr, shell)) {
+        ret = check_break_loop(ptr, shell);
+        if (ret > 0 && bool_cmd[i + ret] != NULL) {
+            i += ret;
+        } else if (ret == -1) {
             break;
         }
     }
