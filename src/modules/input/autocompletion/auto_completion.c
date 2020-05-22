0acb47b3a02459c391bb98885b0ed23b9bb0f480
Vincent Andrieu <vincent.andrieu@epitech.eu>
2020-05-22 11:14:11 +0200

    Edit rename files types

--- src/modules/input/autocompletion/auto_completion.c
+++ src/modules/input/autocompletion/auto_completion.c
@@ -10,7 +10,7 @@
 #include "my_key.h"
 #include "autocompletion.h"
 
-static void free_list(file_t *files)
+static void free_list(file_list_t *files)
 {
     if (files == NULL)
         return;
@@ -19,14 +19,14 @@ static void free_list(file_t *files)
     free(files);
 }
 
-static int get_list_size(file_t *files, int i)
+static int get_list_size(file_list_t *files, int i)
 {
     if (files == NULL || i > 1)
         return i;
     return get_list_size(files->next, i + 1);
 }
 
-static void print_list(file_t *files)
+static void print_list(file_list_t *files)
 {
     if (files == NULL)
         return;
@@ -38,7 +38,7 @@ static void print_list(file_t *files)
     print_list(files->next);
 }
 
-static int does_show_list(file_t *files, int size)
+static int does_show_list(file_list_t *files, int size)
 {
     if (size > 1) {
         my_putchar('\n');
@@ -50,7 +50,7 @@ static int does_show_list(file_t *files, int size)
 
 int check_tab(char **line, size_t *pos, int ch, env_t *env)
 {
-    file_t *files;
+    file_list_t *files;
     int size;
     int exit_value;
     char *backup;
