791bd8140dc60fc05b61f1ec84dd40068f3c2dc5
RACAUD Simon <simonracaud@gmail.com>
2020-05-23 18:47:59 +0200

    Fix(norme) ctrl_d.c

--- src/modules/input/ctrl_d.c
+++ src/modules/input/ctrl_d.c
@@ -10,16 +10,15 @@
 bool ctrl_d_manage(int ch, shell_t *shell, char *line, size_t *pos)
 {
     if (ch == 4) {
-        if (line && line[0] == '\0')
+        if (line && line[0] == '\0') {
             free(line);
+        }
         if (!line || line[0] == '\0') {
             return true;
-        }
-        else if (*pos == strlen(line)) {
+        } else if (*pos == strlen(line)) {
             fprintf(stdout, "\n");
             display_line(shell, line, *pos);
-        }
-        else {
+        } else {
             rm_char(line, pos, true);
         }
     }
