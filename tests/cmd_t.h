6999b5bffafd984a3b0c17538c8daa1ccab82858
RACAUD Simon <simonracaud@gmail.com>
2020-05-23 13:47:39 +0200

    Tests(rename tests_unit => tests directory)

---
+++ tests/cmd_t.h
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2020
+** PSU_42sh_2019
+** File description:
+** Created: 2020-04-03T10:50:19+02:00 | Author: simon
+*/
+
+#ifndef H_CMD_T
+#define H_CMD_T
+
+#include "parser_t.h"
+
+typedef struct cmd {
+    token_t *begin;
+    token_t *end;
+    size_t size;
+} cmd_t;
+
+#endif
